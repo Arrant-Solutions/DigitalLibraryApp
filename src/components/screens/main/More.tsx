67b494ebd2409733ec62ee2f84379d942a88ee2b
WillyMilimo <milicodemoonga@gmail.com>
2021-08-21 21:26:54 +0200

    more sorted

--- src/components/screens/main/More.tsx
+++ src/components/screens/main/More.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { Avatar, Button, Icon } from 'react-native-elements'
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Pressable,
+  TouchableOpacity
+} from 'react-native'
+import { Avatar, Icon } from 'react-native-elements'
 import { ScrollView } from 'react-native-gesture-handler'
 import LinearGradient from 'react-native-linear-gradient'
-import TouchableScale from 'react-native-touchable-scale'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { useAppSelector } from '../../../redux/hooks'
-import { selectAuth } from '../../../redux/slices/authSlice'
+import { useAppDispatch, useAppSelector } from '../../../redux/hooks'
+import { logout, selectAuth } from '../../../redux/slices/authSlice'
 import Header from '../../common/Header'
 import {
   black,
@@ -15,7 +21,7 @@ import {
   greys,
   purple,
   purplePallet,
-  stretchedBox
+  blue
 } from '../../common/style'
 import { Dimensions } from 'react-native'
 import { BlurView } from '@react-native-community/blur'
@@ -23,9 +29,11 @@ import { BlurView } from '@react-native-community/blur'
 const { height, width } = Dimensions.get('window')
 
 const More = () => {
+  const dispatch = useAppDispatch()
   const { user } = useAppSelector(selectAuth)
   return (
-    <SafeAreaView>
+    <View style={{ flex: 1 }}>
+      <Header title="More" containerStyle={{ borderBottomWidth: 0 }} />
       <LinearGradient
         colors={[
           purplePallet.purpleDarker,
@@ -36,8 +44,7 @@ const More = () => {
         end={{ x: 1, y: 1 }}
         useAngle
         angle={110}
-        style={stretchedBox}>
-        <Header title="More" containerStyle={{ borderBottomWidth: 0 }} />
+        style={{ display: 'flex', flex: 1 }}>
         <View
           style={{
             height: 30,
@@ -45,7 +52,6 @@ const More = () => {
           }}></View>
         <View
           style={{
-            height: 0,
             backgroundColor: 'transparent',
             borderStyle: 'solid',
             borderRightWidth: Dimensions.get('window').width,
@@ -53,7 +59,42 @@ const More = () => {
             borderRightColor: 'transparent',
             borderTopColor: purple[60]
           }}></View>
-        <ScrollView style={styles.scrollView}>
+        <ScrollView
+          style={{
+            display: 'flex',
+            flex: 1,
+            backgroundColor: '#00000000',
+            position: 'absolute',
+            height: '100%'
+          }}>
+          <View
+            style={{
+              position: 'absolute',
+              backgroundColor: purple[60],
+              justifyContent: 'center',
+              alignItems: 'center',
+              alignSelf: 'center',
+              zIndex: 999,
+              top: 45,
+              padding: 2,
+              borderRadius: 70
+            }}>
+            <Avatar
+              containerStyle={{
+                backgroundColor: purple[40],
+                padding: 1
+              }}
+              size={120}
+              rounded
+              source={
+                user.avatar
+                  ? {
+                      uri: user.avatar
+                    }
+                  : require('../../../../assets/images/girly.jpg')
+              }
+            />
+          </View>
           <View style={styles.shadowed}>
             <View
               style={{
@@ -61,7 +102,11 @@ const More = () => {
                 flex: 1
               }}>
               <Text
-                style={{ fontSize: 30, textAlign: 'center', color: gold[40] }}>
+                style={{
+                  fontSize: 30,
+                  textAlign: 'center',
+                  color: gold[40]
+                }}>
                 {user.fullname}
               </Text>
               <Text
@@ -81,107 +126,90 @@ const More = () => {
                   paddingHorizontal: '1.5%'
                 }}>
                 <View style={styles.box}>
-                  <Button
-                    type="clear"
-                    style={{ alignItems: 'flex-start' }}
-                    icon={
+                  <TouchableOpacity onPress={() => console.log('pressed')}>
+                    <View style={styles.btnBox}>
                       <Icon
                         type="material-community"
                         name="account-edit-outline"
                         size={25}
                         color={black[60]}
-                        containerStyle={{ marginRight: 10 }}
                       />
-                    }
-                    title="Edit Profile"
-                  />
-                  <Button
-                    type="clear"
-                    style={{ alignItems: 'flex-start' }}
-                    icon={
-                      <Icon
-                        name="credit-card"
-                        size={20}
-                        color={black[60]}
-                        containerStyle={{ marginRight: 10 }}
-                      />
-                    }
-                    title="Payment Details"
-                  />
-                  <Button
-                    type="clear"
-                    style={{ alignItems: 'flex-start' }}
-                    icon={
+                      <Text style={[styles.linkText, { paddingLeft: 7 }]}>
+                        Edit Profile
+                      </Text>
+                    </View>
+                  </TouchableOpacity>
+
+                  <TouchableOpacity onPress={() => console.log('pressed')}>
+                    <View style={styles.btnBox}>
+                      <Icon name="credit-card" size={20} color={black[60]} />
+                      <Text style={[styles.linkText, { paddingLeft: 11 }]}>
+                        Payment Details
+                      </Text>
+                    </View>
+                  </TouchableOpacity>
+
+                  <TouchableOpacity onPress={() => console.log('pressed')}>
+                    <View style={styles.btnBox}>
                       <Icon
                         type="fontisto"
                         name="bar-chart"
                         size={15}
                         color={black[60]}
-                        containerStyle={{ marginRight: 10 }}
                       />
-                    }
-                    title="Activity Reports"
-                  />
-                  <Button
-                    type="clear"
-                    style={{ alignItems: 'flex-start' }}
-                    icon={
+                      <Text style={[styles.linkText, { paddingLeft: 11 }]}>
+                        Activity Reports
+                      </Text>
+                    </View>
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={() => console.log('pressed')}>
+                    <View style={styles.btnBox}>
                       <Icon
                         type="ionicon"
                         name="key-sharp"
                         size={20}
                         color={black[60]}
-                        containerStyle={{ marginRight: 10 }}
                       />
-                    }
-                    title="Reset Password"
-                  />
+                      <Text style={[styles.linkText, { paddingLeft: 11 }]}>
+                        Reset Password
+                      </Text>
+                    </View>
+                  </TouchableOpacity>
                 </View>
-
                 <View style={styles.box}>
                   <Text
                     style={{
                       color: black[70],
                       fontSize: 16,
-                      paddingLeft: 7,
+                      paddingLeft: 15,
                       paddingTop: 7
                     }}>
                     We would like to hear from you
                   </Text>
-                  <Button
-                    containerStyle={{
-                      margin: 0
-                    }}
-                    type="clear"
-                    style={{ alignItems: 'flex-start' }}
-                    title="Give us your feedback"
-                  />
-                </View>
-                <View style={styles.box}>
-                  <Button
-                    containerStyle={{
-                      margin: 0
-                    }}
-                    type="clear"
-                    style={{ alignItems: 'flex-start' }}
-                    title="Terms and Conditions"
-                  />
-                </View>
-                <View style={styles.box}>
-                  <Button
-                    containerStyle={{
-                      margin: 0
-                    }}
-                    type="clear"
-                    style={{ alignItems: 'flex-start' }}
-                    title="Logout"
-                  />
+                  <Text
+                    style={[
+                      styles.linkText,
+                      { paddingBottom: 5, marginTop: 8 }
+                    ]}>
+                    Give us your feedback
+                  </Text>
                 </View>
 
+                <Pressable onPress={() => console.log('pressed')}>
+                  <View style={styles.box}>
+                    <Text style={styles.linkText}>Terms and Conditions</Text>
+                  </View>
+                </Pressable>
+                <Pressable onPress={() => dispatch(logout())}>
+                  <View style={styles.box}>
+                    <Text style={styles.linkText}>Logout</Text>
+                  </View>
+                </Pressable>
                 <Text
                   style={{
                     alignSelf: 'flex-end',
-                    color: black[80]
+                    color: greys[20],
+                    marginTop: 20
                     // padding: 10
                   }}>
                   Version 1.001 (Paul of Tarsus)
@@ -189,35 +217,9 @@ const More = () => {
               </View>
             </View>
           </View>
-          <View
-            style={{
-              backgroundColor: 'transparent',
-              justifyContent: 'center',
-              alignItems: 'center'
-            }}>
-            <BlurView blurType="light" blurAmount={30} style={styles.blurView}>
-              <Avatar
-                containerStyle={{
-                  backgroundColor: purple[40],
-                  padding: 1,
-                  alignSelf: 'center',
-                  position: 'absolute'
-                }}
-                size={120}
-                rounded
-                source={
-                  user.avatar
-                    ? {
-                        uri: user.avatar
-                      }
-                    : require('../../../../assets/images/girly.jpg')
-                }
-              />
-            </BlurView>
-          </View>
         </ScrollView>
       </LinearGradient>
-    </SafeAreaView>
+    </View>
   )
 }
 
@@ -274,7 +276,7 @@ const styles = StyleSheet.create({
     borderTopLeftRadius: 1000,
     borderTopRightRadius: 1000,
     marginTop: 10,
-    position: 'absolute',
+    // position: Platform.OS === 'android' ? 'relative' : 'absolute',
     width: width * 1.5,
     height: height,
     top: 130,
@@ -286,5 +288,16 @@ const styles = StyleSheet.create({
     marginVertical: 10,
     borderRadius: 8,
     padding: 8
+  },
+  btnBox: {
+    flexDirection: 'row',
+    paddingHorizontal: 10,
+    alignItems: 'center'
+  },
+  linkText: {
+    color: blue[90],
+    fontSize: 18,
+    paddingHorizontal: 15,
+    paddingBottom: 2
   }
 })
