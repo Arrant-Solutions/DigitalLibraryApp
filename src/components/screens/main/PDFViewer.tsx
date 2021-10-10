660270891c0b22ed6bc5c012a1ab6118a40023b4
WillyMilimo <milicodemoonga@gmail.com>
2021-10-10 17:37:23 +0200

    page change sorted

--- src/components/screens/main/PDFViewer.tsx
+++ src/components/screens/main/PDFViewer.tsx
@@ -1,9 +1,10 @@
 import {useRoute, RouteProp, useNavigation} from '@react-navigation/native'
-import PDF from 'react-native-pdf'
+import Pdf from 'react-native-pdf'
 import Slider from '@react-native-community/slider'
-import React, {useEffect, useState} from 'react'
-import {Dimensions, StyleSheet, View} from 'react-native'
+import React, {useEffect, useRef, useState} from 'react'
+import {Dimensions, StyleSheet, Text, View} from 'react-native'
 import {ResourceItemT} from 'types/Resource'
+import {pcl} from '../common/style'
 
 type ParamList = {
   PDFViewer: {
@@ -15,6 +16,9 @@ const PDFViewer = () => {
   const {params} = useRoute<RouteProp<ParamList, 'PDFViewer'>>()
   const [resource] = useState(params?.resource)
   const [scale, setScale] = useState(1)
+  const [currentPage, setCurrentPage] = useState(1)
+  const [totalPages, setTotalPages] = useState(0)
+  const pdf = useRef<Pdf | null>(null)
   // console.log(params, resource)
 
   //   useEffect(() => {
@@ -26,14 +30,18 @@ const PDFViewer = () => {
 
   return (
     <View style={styles.container}>
-      <PDF
+      <Pdf
+        ref={pdf}
         scale={scale}
         source={{uri: resource?.resource_url}}
+        // page={currentPage}
         onLoadComplete={(numberOfPages, filePath) => {
           console.log(`number of pages: ${numberOfPages}`)
+          setTotalPages(numberOfPages)
         }}
         onPageChanged={(page, numberOfPages) => {
           console.log(`current page: ${page}`)
+          setCurrentPage(page)
         }}
         onError={error => {
           console.log(error)
@@ -44,15 +52,30 @@ const PDFViewer = () => {
         fitPolicy={1}
         style={styles.pdf}
       />
-      <View style={styles.controls}>
-        <Slider
-          style={{width: 200, height: 40}}
-          minimumValue={1}
-          maximumValue={3}
-          minimumTrackTintColor="#FFFFFF"
-          maximumTrackTintColor="#000000"
-        />
-      </View>
+      {totalPages > 0 && (
+        <View style={styles.controls}>
+          <View style={styles.pagination}>
+            <Slider
+              tapToSeek={true}
+              style={{width: '100%', height: 40}}
+              minimumValue={1}
+              maximumValue={totalPages}
+              step={1}
+              minimumTrackTintColor="#FFFFFF"
+              maximumTrackTintColor="#000000"
+              value={currentPage}
+              onSlidingComplete={value => {
+                console.log(`sliding done: ${value}`)
+                //   setCurrentPage(value)
+                pdf?.current?.setPage(value)
+                return
+              }}
+              onValueChange={value => console.log(`changed to: ${value}`)}
+            />
+            <Text style={styles.pages}>{`${currentPage} / ${totalPages}`}</Text>
+          </View>
+        </View>
+      )}
     </View>
   )
 }
@@ -78,5 +101,17 @@ const styles = StyleSheet.create({
     bottom: 0,
     zIndex: 999,
     backgroundColor: 'rgba(0,0,0,.7)',
+  },
+  pagination: {
+    flexDirection: 'column',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  pages: {
+    fontFamily: 'Roboto-Regular',
+    color: pcl.background,
+    fontSize: 17,
+    marginTop: 10,
   },
 })
