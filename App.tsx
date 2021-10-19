7c517b24b07c218192b516368203d6deae7e9e5a
WillyMilimo <milicodemoonga@gmail.com>
2021-10-19 14:50:06 +0200

    fixing no bundle url bug

--- App.tsx
+++ App.tsx
@@ -1,13 +1,17 @@
-import React, {useState} from 'react'
+import React from 'react'
 import 'react-native-gesture-handler'
-import {NavigationContainer, DefaultTheme} from '@react-navigation/native'
+import {
+  NavigationContainer,
+  DefaultTheme,
+  NavigationContainerRef,
+} from '@react-navigation/native'
+import analytics from '@react-native-firebase/analytics'
 import MainNavigation from 'components/MainNavigation'
 import {pcl} from 'components/screens/common/style'
 import {StyleSheet} from 'react-native'
 import {Provider} from 'react-redux'
 import {store} from './src/redux/store'
-import SplashScreen from 'react-native-splash-screen'
-import { SafeAreaProvider } from 'react-native-safe-area-context'
+import {SafeAreaProvider} from 'react-native-safe-area-context'
 
 const MyTheme = {
   ...DefaultTheme,
@@ -24,10 +28,33 @@ const MyTheme = {
 }
 
 const App = () => {
+  const routeNameRef = React.useRef()
+  const navigationRef =
+    React.useRef<
+      React.Ref<NavigationContainerRef<ReactNavigation.RootParamList>>
+    >()
+
   return (
     <SafeAreaProvider>
       <Provider store={store}>
-        <NavigationContainer>
+        <NavigationContainer
+          ref={navigationRef => void}
+          onReady={() => {
+            routeNameRef.current = navigationRef.current.getCurrentRoute().name
+          }}
+          onStateChange={async () => {
+            const previousRouteName = routeNameRef.current
+            const currentRouteName =
+              navigationRef?.current?.getCurrentRoute().name
+
+            if (previousRouteName !== currentRouteName) {
+              await analytics().logScreenView({
+                screen_name: currentRouteName,
+                screen_class: currentRouteName,
+              })
+            }
+            routeNameRef.current = currentRouteName
+          }}>
           <MainNavigation />
         </NavigationContainer>
       </Provider>
